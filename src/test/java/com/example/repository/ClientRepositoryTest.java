4e580791bdec51e1ecd436931e58cd10eb463aa0
starkov <post.starkov@gmail.com>
2016-08-17 23:16:57 +0300

    It adds ClienRepository tests

--- src/test/java/com/example/repository/ClientRepositoryTest.java
+++ src/test/java/com/example/repository/ClientRepositoryTest.java
@@ -1,8 +1,36 @@
 package com.example.repository;
 
-import com.example.AbstractTest;
+import com.example.AbstractRepositoryTest;
+import com.example.entity.ClientEntity;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 
-public class ClientRepositoryTest extends AbstractTest {
-//TODO
+public class ClientRepositoryTest extends AbstractRepositoryTest {
+
+    public static final String EMAIL = "newEmail@bk.com";
+
+    @Test
+    public void save() {
+        ClientEntity client = createClient();
+        assertNotNull(client);
+    }
+
+    @Test
+    public void update() {
+        ClientEntity client = createClient();
+        client.setEmail(EMAIL);
+        client = clientRepository.save(client);
+        assertNotNull(client);
+        assertEquals(EMAIL, client.getEmail());
+    }
+
+    @Test
+    public void delete() {
+        ClientEntity client = createClient();
+        clientRepository.delete(client);
+        client = clientRepository.findOne(client.getId());
+        assertNull(client);
+    }
 }
