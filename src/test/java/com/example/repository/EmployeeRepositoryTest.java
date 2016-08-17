c311f40bb56216333581d107cd67792ce25e8ed9
starkov <post.starkov@gmail.com>
2016-08-17 23:17:18 +0300

    It adds EmployeeRepository tests

--- src/test/java/com/example/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/repository/EmployeeRepositoryTest.java
@@ -1,7 +1,36 @@
 package com.example.repository;
 
-import com.example.AbstractTest;
+import com.example.AbstractRepositoryTest;
+import com.example.entity.EmployeeEntity;
+import org.junit.Test;
 
-public class EmployeeRepositoryTest extends AbstractTest {
-//TODO
+import static org.junit.Assert.*;
+
+public class EmployeeRepositoryTest extends AbstractRepositoryTest {
+
+
+    public static final String EMAIL = "newEmail@bk.com";
+
+    @Test
+    public void save() {
+        EmployeeEntity employee = createEmployee();
+        assertNotNull(employee);
+    }
+
+    @Test
+    public void update() {
+        EmployeeEntity employee = createEmployee();
+        employee.setEmail(EMAIL);
+        employee = employeeRepository.save(employee);
+        assertNotNull(employee);
+        assertEquals(EMAIL, employee.getEmail());
+    }
+
+    @Test
+    public void delete() {
+        EmployeeEntity employee = createEmployee();
+        employeeRepository.delete(employee);
+        employee = employeeRepository.findOne(employee.getId());
+        assertNull(employee);
+    }
 }
